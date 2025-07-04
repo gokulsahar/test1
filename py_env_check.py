d1a3eabfcb80a57997f4671f584bec71aba520f9
gokulsahar <gokulsahar@gmail.com>
2025-07-04 23:13:52 +0530

    pkg verify

---
+++ py_env_check.py
@@ -0,0 +1,20 @@
+pkgs = [
+    # runtime
+    "ruamel.yaml", "pydantic", "click", "networkx", "pandas", "numpy", "dask",
+    "msgpack", "requests", "typing_extensions", "toolz", "cloudpickle",
+    "partd", "fsspec", "python_dateutil", "tzdata", "pytz", "jsonschema", "graphviz",
+    # dev / test
+    "pytest", "pytest_cov", "black", "isort", "mypy", "pre_commit",
+]
+
+missing = []
+for mod in pkgs:
+    try:
+        __import__(mod.replace("-", "_"))
+    except ImportError:
+        missing.append(mod)
+
+if not missing:
+    print("All DataPY packages present!")
+else:
+    print("Missing:", ", ".join(missing))
